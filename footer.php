51fe1f32007c75f825287557757491349f00c246
Sekander Badsha <sekander@Sekanders-MacBook-Pro.local>
2016-05-31 14:15:06 +0600

    initial commit

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package olpo
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<p>Hand Creafted by - Sekander Badsha </p>
+			None of the contents on this site is copyrighted. You are free to use and abuse anything found under this domain.
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package olpo
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+			get_template_part( 'template-parts/content', get_post_format() );
+
+			the_post_navigation( array(
+				'prev_text' => __( '&larr; Previous Post: %title', 'olpo' ),
+    			'next_text' => __( 'Next Post: %title &rarr;', 'olpo' ),
+				));
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
